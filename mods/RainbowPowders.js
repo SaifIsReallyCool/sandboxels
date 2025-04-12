f1660027c778740e99f84e41a3fad1a34b20d3b5
SaifIsReallyCool <Saifsaleh12341234@outlook.com>
2025-04-13 09:33:33 +1000

    Create RainbowPowders.js

---
+++ mods/RainbowPowders.js
@@ -0,0 +1,18 @@
+elements.Red = {
+	color: "#ff0000",
+	behavior: behaviors.POWDER,
+	category: "Powders",
+	state: "solid",
+}
+elements.Orange = {
+	color: "#ffa500",
+	behavior: behaviors.POWDER,
+	category: "Powders",
+	state: "solid",
+}
+elements.Yellow = {
+	color: "#ffff00",
+	behavior: behaviors.POWDER,
+	category: "Powders",
+	state: "solid",
+}
